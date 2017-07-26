ff8e250b9401b6854c3d95f0e5886a6fa01ed714
fenglish <asuka.ochi@ft.com>
2017-07-26 15:54:20 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function (req, res) {
+	res.send('Hello!');
+})
+
+app.listen(3000, function () {
+	console.log('Server is listening on port 3000. Ready to accept requests!');
+})
