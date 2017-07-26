3f5b7a8c107008ba445b89891a676aa6c56ab2ab
fenglish <asuka.ochi@ft.com>
2017-07-26 16:05:00 +0100

    add some routing

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
+var comment = "fun!"
+
 app.get("/", function (req, res) {
 	res.send('Hello!');
-})
+});
+
+app.get("/chocolate", function (req,res) {
+	res.send('dark!');
+});
+
+app.get('/node', function (req,res) {
+	res.send(comment);
+});
+
+app.get('/girls', function (req, res) {
+	res.send(comment);
+});
 
 app.listen(3000, function () {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
-})
+});
