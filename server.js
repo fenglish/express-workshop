8502c963984c5552d7b624a6332936aa58b75cb0
fenglish <asuka.ochi@ft.com>
2017-07-26 17:26:50 +0100

    display recent posts

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.post('/create-post', function (req, res) {
 
 });
 
+app.get('/get-posts', function (req, res) {
+	res.sendFile(__dirname + '/data/posts.json');
+});
+
 app.listen(3000, function () {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
 });
