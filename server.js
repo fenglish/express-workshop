47b4e007eef7fd0788792bac1f4ddb1bb1a12492
fenglish <asuka.ochi@ft.com>
2017-07-26 16:12:35 +0100

    add middleware

--- server.js
+++ server.js
@@ -3,21 +3,7 @@ var app = express();
 
 var comment = "fun!"
 
-app.get("/", function (req, res) {
-	res.send('Hello!');
-});
-
-app.get("/chocolate", function (req,res) {
-	res.send('dark!');
-});
-
-app.get('/node', function (req,res) {
-	res.send(comment);
-});
-
-app.get('/girls', function (req, res) {
-	res.send(comment);
-});
+app.use(express.static("public"));
 
 app.listen(3000, function () {
 	console.log('Server is listening on port 3000. Ready to accept requests!');
