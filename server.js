53961a1d53574e1d5cd9509b87d7ac373dd8a22c
fenglish <asuka.ochi@ft.com>
2017-07-26 17:19:11 +0100

    add a new post

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var app = express();
-var formidable = require('express-formidable');
+const express = require('express');
+const app = express();
+const formidable = require('express-formidable');
+const fs = require('fs');
+
 
 app.use(express.static("public"));
 app.use(formidable());
 
 app.post('/create-post', function (req, res) {
-	console.log(req.fields);
+
+	fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+		let currentBlogPosts = JSON.parse(file);
+		let timestamp = Date.now();
+		let newBlogPost = req.fields.blogpost;
+		currentBlogPosts[timestamp] = newBlogPost;
+		let allBlogPosts = JSON.stringify(currentBlogPosts);
+
+		fs.writeFile(__dirname + '/data/posts.json', allBlogPosts, function (error) {
+		});
+	});
+
 });
 
 app.listen(3000, function () {
